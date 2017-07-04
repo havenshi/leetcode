a7e0ed33c76defdcbf0ebdf32bdb57e7e12072a0
Haven Shi <haiwen.shi01@gmail.com>
2017-07-03 22:03:49 -0500

    queens

---
+++ 51. N-Queens.py
@@ -0,0 +1,46 @@
+class Solution(object):
+    def solveNQueens(self, n):
+        """
+        :type n: int
+        :rtype: List[List[str]]
+        """
+        array = [-1 for i in range(n)]
+        self.res = []
+        for i in range(n):
+            self.dfs([i] + array[1:], 1)
+        return self.res
+        # result = []
+        # for item in self.res:
+        #     board = [['.'] * n for _ in range(n)]
+        #     for i in range(n):
+        #         board[i][item[i]] = 'Q'
+        #     tmp = []
+        #     for i in range(n):
+        #         tmp.append(''.join(board[i]))
+        #     result.append(tmp)
+        # return result
+
+    def dfs(self, array, count):
+        n = len(array)
+        if count == n:
+            self.res.append(array)
+            print array,self.res
+            return True
+        flag = False
+        for i in range(n):
+            array[count] = i
+            if self.isValid(array, i, count) and self.dfs(array, count + 1):
+                flag = True
+                # set 'flag value' rather than return True directly. Or if several i satisfy, 'for' loop  will end at the first time of True. It can miss some possibility.
+            array[count] = -1
+        return flag
+
+    def isValid(self, array, x, count):
+        for i in range(count):
+            if array[i] == x or abs(count - i) == abs(x - array[i]):
+                return False
+        return True
+
+if __name__ == "__main__":
+    print Solution().solveNQueens(4)
+
