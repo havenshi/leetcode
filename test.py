d61607c9f4cbdddd20d2e4e167967c37724ee344
Haven Shi <haiwen.shi01@gmail.com>
2018-02-18 21:16:16 -0600

    third shua

--- test.py
+++ test.py
@@ -1,23 +1,39 @@
-class Solution(object):
-    def trapRainWater(self, heightMap):
-        """
-        :type heightMap: List[List[int]]
-        :rtype: int
-        """
-        m = len(heightMap)
-        n = len(heightMap[0]) if m else 0
-
-        peakMap = [[float('inf')] * n for _ in range(m)]
-
-        q = []
-        for x in range(m):
-            for y in range(n):
-                if x in (0, m - 1) or y in (0, n - 1):
-                    peakMap[x][y] = heightMap[x][y]
-                    q.append((x, y))
-        print peakMap
-if __name__ == "__main__":
-    print Solution().trapRainWater([[1,4,3,1,3,2],[3,2,1,3,2,4],[2,3,3,2,3,1]])
+# Complete the function below.
+# this is dfs and combination
+# we can use from scipy.special import comb in other IDE, but it's not a basic module so we can't use here
+import operator
 
 
+def dfs(grid, i, j):
+    if i > len(grid) - 1 or i < 0 or j > len(grid[0]) - 1 or j < 0:
+        return
+    if grid[i][j] == 'N':
+        return
+    grid[i][j] == 'N'
+    dfs(grid, i + 1, j)
+    dfs(grid, i, j + 1)
+    dfs(grid, i - 1, j)
+    dfs(grid, i, j - 1)
 
+
+def combination(count, i):
+    return reduce(operator.mul, range(count - i + 1, count + 1)) / reduce(operator.mul, range(1, i + 1))
+
+
+def Group(grid):
+    ans = 0
+    if not len(grid):
+        ans = 0
+    else:
+        m = len(grid)
+        n = len(grid[0])
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 'Y':
+                    dfs(grid, i, j)
+                    ans += 1
+
+    count = 0
+    for i in range(ans / 2 * 2 + 1):
+        count += combination(count, i)
+    return count
