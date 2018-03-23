b6f70808c6f0ed49fa922b665a3aa98b3c43f5ca
Haven Shi <haiwen.shi01@gmail.com>
2018-03-23 11:05:50 -0500

    update to 300

--- test.py
+++ test.py
@@ -1,18 +1,2 @@
 # Time:  O(n)
 # Space: O(1)
-def zigzag(matrix):
-    m = len(matrix)
-    row, col = 0, 0
-    ans = []
-    while row < m:
-        while col < len(matrix[row]):
-            if col < len(matrix[row]):
-                ans.append(matrix[row][col])
-            col += 1
-        col = 0
-        row += 1
-    return ans
-
-print zigzag([[1,2,3],
-               [4,5,6,7],
-               [8,9]])
